c6fc19efa676fb24d1f033868419d5063996bc93
HP <HP@DESKTOP-TVBSRNK>
2024-06-04 18:57:51 +0200

    Add project files.

---
+++ Fo/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using Dolgozat.Command;
+using Dolgozat.Nezetek;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolgozat.Fo
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+
+        private object currentView;
+
+        public object CurrentView
+        {
+            get { return currentView; }
+            set { currentView = value; OnPropertyChanged(); }
+        }
+
+        HomeView homeView;
+        AbraKilenc abraKilenc;
+        SqlView sqlView;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public RelayCommand openMain { get; }
+        public RelayCommand kilencesAbra { get; }
+        public RelayCommand sqlNezet { get; }
+
+        public MainViewModel()
+        {
+            homeView = new HomeView();
+            abraKilenc = new AbraKilenc();
+            sqlView = new SqlView();
+
+            openMain = new RelayCommand(X => CurrentView = homeView);
+            kilencesAbra = new RelayCommand(X => CurrentView = abraKilenc);
+            sqlNezet = new RelayCommand(X => CurrentView = sqlView);
+
+            CurrentView = homeView;
+        }
+        private void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ Nezetek/SqlView.xaml.cs
@@ -0,0 +1,79 @@
+﻿using Sql.SQL;
+using System.Windows;
+using System.Windows.Controls;
+using static Sql.Models.Emberek;
+
+namespace Dolgozat.Nezetek
+{
+    /// <summary>
+    /// Interaction logic for SqlView.xaml
+    /// </summary>
+    public partial class SqlView : UserControl
+    {
+        private readonly AdatbazisContext _context;
+        private User _selectedUser;
+        public SqlView()
+        {
+            InitializeComponent();
+            _context = new AdatbazisContext();
+            LoadData();
+        }
+
+
+
+        private void LoadData()
+        {
+            var users = _context.User.ToList();
+            listBox.ItemsSource = users;
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (listBox.SelectedIndex != -1)
+            {
+                _selectedUser = (User)listBox.SelectedItem;
+                nameTextBox.Text = _selectedUser.Name;
+                postTextBox.Text = _selectedUser.PostCode;
+                buildingTextBox.Text = _selectedUser.Building;
+                positionTextBox.Text = _selectedUser.Position;
+                transportationTextBox.Text = _selectedUser.Transport;
+            }
+        }
+
+        private async void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (_selectedUser != null)
+            {
+                _selectedUser.Name = nameTextBox.Text;
+                _selectedUser.PostCode = postTextBox.Text;
+                _selectedUser.Building = buildingTextBox.Text;
+                _selectedUser.Position = positionTextBox.Text;
+                _selectedUser.Transport = transportationTextBox.Text;
+
+                _context.User.Update(_selectedUser);
+                await _context.SaveChangesAsync();
+                LoadData();
+            }
+        }
+
+        private async void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (_selectedUser != null)
+            {
+                _context.User.Remove(_selectedUser);
+                await _context.SaveChangesAsync();
+                LoadData();
+                ClearFields();
+            }
+        }
+
+        private void ClearFields()
+        {
+            nameTextBox.Text = "";
+            postTextBox.Text = "";
+            buildingTextBox.Text = "";
+            positionTextBox.Text = "";
+            transportationTextBox.Text = "";
+        }
+    }
+}
